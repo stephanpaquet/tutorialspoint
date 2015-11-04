a977f3318b657330415cda92a06f60ffecaa4bca
spaquet <stephan.paquet@duproprio.com>
2015-11-04 10:32:49 -0500

    Add lesson 4

---
+++ Node.js-Tutorial/04-First-Application/README.md
@@ -0,0 +1,13 @@
+# Node.js - First Application
+
+A Node.js application consists of following three important parts −
+
+- **Import required modules** − We use require directive to load a Node.js module.
+- **Create server** − A server which will listen to client's request similar to Apache HTTP Server.
+- **Read request and return response** − server created in earlier step will read HTTP request made by client which can be a browser or console and return the response.
+
+
+```bash
+ node main.js
+ ```
+ 
---
+++ Node.js-Tutorial/04-First-Application/main.js
@@ -0,0 +1,18 @@
+/*global require, console */
+var http = require("http");
+
+console.log(http);
+http.createServer(function (request, response) {
+    "use strict";
+
+    // Send the HTTP header
+    // HTTP Status: 200 : OK
+    // Content Type: text/plain
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+
+    // Send the response body as "Hello World"
+    response.end('Hello World\n');
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
